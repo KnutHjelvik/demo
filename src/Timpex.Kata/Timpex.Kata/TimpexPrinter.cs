ad04df7173a6640dc14d5462a6de5e97b6a4d5b3
Knut E. Hjelvik <Knut.Hjelvik@Timpex.no>
2014-11-12 15:47:20 +0100

    Task 1

---
+++ src/Timpex.Kata/Timpex.Kata/TimpexPrinter.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Timpex.Kata
+{
+    public class TimpexPrinter
+    {
+        private int _counter;
+
+        public string Print()
+        {
+            _counter++;
+            if (_counter % 3 == 0 & _counter % 5 == 0)
+                return "timpex";
+            if (_counter % 3 == 0)
+                return "tim";
+            if (_counter % 5 == 0)
+                return "pex";
+            return _counter.ToString();
+        }
+    }
+}
---
+++ src/Timpex.Kata/Timpex.Kata/_Spec/_TimpexPrinter/TimpexPrinterTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Timpex.Kata._Spec._TimpexPrinter
+{
+    [TestFixture]
+    public class TimpexPrinterTest
+    {
+        private TimpexPrinter _printer = new TimpexPrinter();        
+        
+        [TestFixtureSetUp]
+        public void When_Printing()
+        {            
+        }
+
+        [Test]
+        public void It_Should_Return_Expected()
+        {
+            Assert.AreEqual("1", _printer.Print());
+            Assert.AreEqual("2", _printer.Print());
+            Assert.AreEqual("tim", _printer.Print());
+            Assert.AreEqual("4", _printer.Print());
+            Assert.AreEqual("pex", _printer.Print());
+            Assert.AreEqual("tim", _printer.Print());
+            Assert.AreEqual("7", _printer.Print());
+            Assert.AreEqual("8", _printer.Print());
+            Assert.AreEqual("tim", _printer.Print());
+            Assert.AreEqual("pex", _printer.Print());
+            Assert.AreEqual("11", _printer.Print());
+            Assert.AreEqual("tim", _printer.Print());
+            Assert.AreEqual("13", _printer.Print());
+            Assert.AreEqual("14", _printer.Print());
+            Assert.AreEqual("timpex", _printer.Print());
+
+        }
+    }
+}
